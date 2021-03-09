534fb29da902cea680416ac5d4a5b6d49b42b3df
Jeff Wright <Jeff.Wright@gmail.com>
2021-03-09 11:40:21 -0500

    Updated gitignore. Added verbose option.Added exception handling for input files with commas.

--- apg.py
+++ apg.py
@@ -2,17 +2,21 @@
 Description:
     apg.py: Generate audio program of custom phrases
 
-    User populates a text file with plain-text phrases, and accompanying
-    inter-phrase durations. Each line of the file constitutes one phrase to be
-    spoken, followed by a semicolon, followed by a silence duration in seconds.
+    User populates a semicolon-separated text file with plain-text phrases,
+    and accompanying inter-phrase durations. Each line of the file
+    constitutes one phrase to be spoken, followed by a silence duration in
+    seconds.
 
-    The script generates and saves a single MP3 file ("program.mp3").
+    The script generates and saves a single MP3 file. The base name of the MP3
+    file is the same as the specified input file. So, for example, if the
+    script is given input file "phrases.txt", the output file will be
+    "phrases.mp3".
 
     Optionally, the script can play out the entire program (consisting of each
     phrase and its corresponding silence interval), using the "play" option.
 
 Usage:
-    apg <phrase_file> [-p | --play]
+    apg <phrase_file> [-p | --play] [-v | --verbose]
     apg -h | --help
     apg -V | --version
 
@@ -39,7 +43,9 @@ Author:
     Jeff Wright <jeff.washcloth@gmail.com>
 """
 import os
+import sys
 from csv import reader
+from pathlib import Path
 from docopt import docopt
 from gtts import gTTS
 from audioplayer import AudioPlayer
@@ -49,13 +55,22 @@ from pydub import AudioSegment
 def main():
     args = docopt(__doc__, version="1.1.0")
     phrase_file = args["<phrase_file>"]
+    save_file = Path(phrase_file).stem + ".mp3"
     play = args["--play"]
+    verbose = args["--verbose"]
 
     with open(phrase_file, "r") as read_obj:
         combined = AudioSegment.empty()
         csv_reader = reader(read_obj)
         for row in csv_reader:
-            phrase, interval = row[0].split(";")
+            try:
+                print(row) if verbose else None
+                phrase, interval = row[0].split(";")
+            except Exception as e:
+                print("Error parsing input file as CSV:")
+                print(row) if verbose else None
+                print(e.args)
+                sys.exit()
             speech = gTTS(phrase)
             speech.save("phrase.mp3")
             speech = AudioSegment.from_file("phrase.mp3", format="mp3")
@@ -63,10 +78,10 @@ def main():
             os.remove("phrase.mp3")
             silence = AudioSegment.silent(duration=1000 * int(interval))
             combined += silence
-        combined.export("program.mp3", format="mp3")
+        combined.export(save_file, format="mp3")
 
     if play:
-        AudioPlayer("program.mp3").play(block=True)
+        AudioPlayer(save_file).play(block=True)
 
 
 if __name__ == "__main__":
