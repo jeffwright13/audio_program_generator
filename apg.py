2d70de5396eb76e0b9f381c1712045d9909c7604
Jeff Wright <Jeff.Wright@gmail.com>
2021-03-13 16:42:37 -0500

    Got basic mixing done. Needs fading.

--- apg.py
+++ apg.py
@@ -14,31 +14,31 @@ Description:
 
     Optionally, the script can play out the entire program (consisting of each
     phrase and its corresponding silence interval), using the "play" option.
+
 Usage:
-    apg [options] <phrase_file> [output_file]
-    apg [options] mix <phrase_file> <wav_file> [output_file]
+    apg [options] <phrase_file>
+    apg [options] mix <phrase_file> <wav_file>
     apg -V --version
     apg -h --help
+
 Options:
     -p --play       Play program after generating.
-    -v --verbose    Print to console each line from <phrase_file>.
     -d --debug      Print debug statements to console.
+
 Arguments:
     phrase_file     Name of semi-colon-separated text file containing
                     phrases and silence durations. Do not include
                     commas in this file.
     wav_file        A .wav file to be mixed into the generated program
                     file. Useful for background music/sounds.
-    [output_file]   Name of file to write to dick once generated.
-                    If not specified, it will be the same name as
-                    <the phrase_file>
+
 Examples:
 
 Example <phrase_file> format:
-    Let's meditate...;2
-    Relax your body;60
-    Relax your mind;60
-    BOO!;0
+    Phrase One;2
+    Phrase Two;5
+    Phrase Three;0
+
 Author:
     Jeff Wright <jeff.washcloth@gmail.com>
 """
@@ -52,38 +52,26 @@ from gtts import gTTS
 from audioplayer import AudioPlayer
 from pydub import AudioSegment
 
-debug = False
+
+def mix(segment1, segment2, level1=0, level2=-12):
+    """
+    Mixes two pydub AudioSegments (with individual levels)
+    Returns mixed AudioSegment
+    """
+    return (segment1 + level1).overlay((segment2 + level2), position=0)
 
 
-def mixer(file1, file2, level1=0, level2=0):
-    if debug:
-        print("In mixer()...")
-        print("file1: ", file1, "file2: ", file2)
-
-    speech = AudioSegment.from_file(file1, format="mp3") + level1
-    bkgrnd = AudioSegment.from_file(file2, format="wav") + level2
-
-    return speech.overlay(bkgrnd, position=0)
-
-
-def main():
-    args = docopt(__doc__, version="Audio Program Generator (apg) v1.2.0")
-    phrase_file = args["<phrase_file>"]
-    save_file = Path(phrase_file).stem + ".mp3"
-    play = args["--play"]
-    verbose = args["--verbose"]
-    mix = args["mix"]
-    debug = args["--debug"]
-    if debug:
-        print(args)
-
+def gen_speech(phrase_file):
+    """
+    Generates speech file from a semicolon-separated file
+    Returns Audiosegment
+    """
     with open(phrase_file, "r") as read_obj:
         combined = AudioSegment.empty()
         csv_reader = reader(read_obj)
         for row in csv_reader:
             tempfile = NamedTemporaryFile().name + ".mp3"
             try:
-                print(row) if verbose else None
                 phrase, interval = row[0].split(";")
             except Exception as e:
                 print("Error parsing input file as CSV:")
@@ -97,15 +85,24 @@ def main():
             os.remove(tempfile)
             silence = AudioSegment.silent(duration=1000 * int(interval))
             combined += silence
-        combined.export(save_file, format="mp3")
+    return combined
 
-    if mix:
-        mixed_file = mixer(save_file, args["<wav_file>"])
-        if play:
-            AudioPlayer(mixed_file).play(block=True)
+
+def main():
+    args = docopt(__doc__, version="Audio Program Generator (apg) v1.2.1")
+    save_file = Path(args["<phrase_file>"]).stem + ".mp3"
+    print(args) if args["--debug"] else None
+    speech = gen_speech(args["<phrase_file>"])
+
+    if args["mix"]:
+        bkgnd = AudioSegment.from_file(args["<wav_file>"], format="wav")
+        mixed = mix(speech, bkgnd)
+        mixed.export(save_file, format="mp3")
     else:
-        if play:
-            AudioPlayer(save_file).play(block=True)
+        speech.export(save_file, format="mp3")
+
+    if args["--play"]:
+        AudioPlayer(save_file).play(block=True)
 
 
 if __name__ == "__main__":
