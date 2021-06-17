b997e0e60a78e60f11d88883557719bd36200224
Jeff Wright <jeff.washcloth@gmail.com>
2021-06-17 09:52:41 -0400

    Initial tests.

--- tests/test_apg.py
+++ tests/test_apg.py
@@ -1,5 +1,18 @@
-from apg import __version__
+from pathlib import Path
+from tempfile import TemporaryFile
+import audio_program_generator.apg as apg
 
 
-def test_version():
-    assert __version__ == '1.6.0'
+def test_module(capsys):
+    with capsys.disabled():
+        print("\napg:", dir(apg))
+        assert "main" in dir(apg)
+
+
+def test_class_methods(capsys):
+    with capsys.disabled():
+        fh = TemporaryFile()
+        A = apg.AudioProgramGenerator(fh)
+        print("\nA:", dir(A))
+        assert "_gen_speech" in dir(A)
+        assert "_mix" in dir(A)
