42858456b58b871134a80f50015d7e61a51f3783
JnyJny <erik.oshaughnessy@gmail.com>
2021-10-31 13:52:02 -0500

    Updated tests to use fixtures
    The tests/conftest.py holds fixtures that can be used in the various
    test functions. The directory test/resources contains a `phrases.txt`
    file and a `mustang4.wav` for use in testing the operation of the
    AudioProgramGenerator class.
    
    Updated `test_apg.py` to include tests for creating a
    AudioProgramGenerator with good and bad inputs.
    
    Created `test_main.py` to test the AudioProgramGenerator.apg.main
    function which uses docopt to define and parse arguments. Arguments
    are spoofed by setting `sys.argv` to the arguments to test.

---
+++ tests/conftest.py
@@ -0,0 +1,56 @@
+"""fixtures for testing AudioProgramGenerator
+"""
+
+import io
+import pytest
+import os
+
+from pathlib import Path
+
+import audio_program_generator.apg as apg
+
+
+@pytest.fixture(scope="session")
+def phrase_path() -> Path:
+    """pathlib.Path to a known good phrase file.
+
+    Beginning lyrics of the song "Black Sunshite" by White Zombie.
+    """
+    return Path(__file__).absolute().parent / "resources/phrases.txt"
+
+
+@pytest.fixture(scope="session")
+def sound_path() -> Path:
+    """Path to a known good WAV file.
+
+    mustang4.wav
+    Source: https://freesound.org/people/VacekH/sounds/205507/
+    License: CC0 1.0
+    """
+    return Path(__file__).absolute().parent / "resources/mustang4.wav"
+
+
+@pytest.fixture(scope="session")
+def phrase_file(phrase_path) -> io.StringIO:
+    """StringIO loaded with the contents of `phrase_path`."""
+    return io.StringIO(phrase_path.read_text())
+
+
+@pytest.fixture(scope="session")
+def sound_file(sound_path) -> io.BytesIO:
+    """BytesIO loaded with the contents of `sound_path`."""
+    return io.BytesIO(sound_path.read_bytes())
+
+
+@pytest.fixture(scope="session")
+def output_path(phrase_path) -> Path:
+    """pathlib.Path of the expected output file."""
+    path = Path(phrase_path.with_suffix(".mp3").name)
+    yield path
+    path.unlink(missing_ok=True)
+
+
+@pytest.fixture(scope="session")
+def APG(phrase_file, sound_file) -> apg.AudioProgramGenerator:
+    """An AudioProgramGenerator created with `phrase_file` and `sound_file`."""
+    return apg.AudioProgramGenerator(phrase_file, sound_file)
