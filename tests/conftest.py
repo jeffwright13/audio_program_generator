4740b361a73cb45a36acb37927f57643df49bfbb
Jeff Wright <jeff.washcloth@gmail.com>
2021-11-02 05:49:03 -0600

    Added a bunch of experimental (and possibly useful) Mock code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,17 @@
-"""fixtures for testing AudioProgramGenerator
+"""Fixtures for testing AudioProgramGenerator
 """
-
 import io
 import pytest
-import os
-
-from pathlib import Path
-
 import audio_program_generator.apg as apg
+from pathlib import Path
+from unittest.mock import MagicMock
 
 
 @pytest.fixture(scope="session")
 def phrase_path() -> Path:
     """pathlib.Path to a known good phrase file.
 
-    Beginning lyrics of the song "Black Sunshite" by White Zombie.
+    Beginning lyrics of the song "Black Sunshin e" by White Zombie.
     """
     return Path(__file__).absolute().parent / "resources/phrases.txt"
 
@@ -54,3 +51,75 @@ def output_path(phrase_path) -> Path:
 def APG(phrase_file, sound_file) -> apg.AudioProgramGenerator:
     """An AudioProgramGenerator created with `phrase_file` and `sound_file`."""
     return apg.AudioProgramGenerator(phrase_file, sound_file)
+
+
+#########################
+# Mocks & Static Objects
+#########################
+@pytest.fixture(scope="session")
+def static_phrase_file() -> io.StringIO:
+    return io.StringIO("Static Phrase File: StringIO")
+
+
+@pytest.fixture(scope="session")
+def static_sound_file() -> io.BytesIO:
+    return io.BytesIO(b"Static Sound File: BytesIO")
+
+
+class MockObject:
+    """
+    A generic mock object that takes an arbitrary list of keyword=value
+    pairs, and creates a MagicMock instance which has attributes with
+    names/values for each kw/val pair presented.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        from unittest.mock import MagicMock
+
+        self.mock = MagicMock()
+        for key, val in kwargs.items():
+            exec(f"self.mock.{key} = '{val}'")
+
+
+def mock_anything(**kwargs) -> MagicMock:
+    """ """
+    mock_thing = MagicMock()
+    for key, val in kwargs.items():
+        exec(f"mock_thing.{key} = '''{val}'''")
+    return mock_thing
+
+
+@pytest.fixture(scope="session")
+def mock_gTTS(**kwargs) -> MagicMock:
+    """
+    Mock AudioProgramGenerator instance.
+    <phrase_file> and <sound_file> are hard-coded (static).
+    User passes in arbitrary keyword args; MagicMock is returned with
+    attributes named as kwargs elements.
+    """
+    mockApg = MagicMock()
+    mockApg.phrase_file = static_phrase_file()
+    mockApg.sound_file = static_sound_file()
+
+    for key, val in kwargs.items():
+        exec(f"mockApg.{key} = '''{val}'''")
+
+    return mockApg
+
+
+@pytest.fixture(scope="session")
+def mock_APG(phrase_file: io.StringIO, sound_file: io.BytesIO, **kwargs) -> MagicMock:
+    """
+    Mock AudioProgramGenerator instance.
+    <phrase_file> and <sound_file> are hard-coded (static).
+    User passes in arbitrary keyword args; MagicMock is returned with
+    attributes named as kwargs elements.
+    """
+    mockApg = MagicMock()
+    mockApg.phrase_file = static_phrase_file()
+    mockApg.sound_file = static_sound_file()
+
+    for key, val in kwargs.items():
+        exec(f"mockApg.{key} = '{val}'")
+
+    return mockApg
