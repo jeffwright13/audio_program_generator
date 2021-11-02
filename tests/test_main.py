e1e90fa675701bfabab1fe6f96fed1b0ceb8e113
Jeff Wright <jeff.washcloth@gmail.com>
2021-11-02 05:49:34 -0600

    Tests still broken.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,27 +1,26 @@
-from pathlib import Path
-
-import pytest
 import sys
-
-
-from audio_program_generator.apg import main as apg_main
+import pytest
+from pathlib import Path
 from docopt import DocoptExit
+from audio_program_generator.apg import main as apg_main
 
 
 def test_apg_main_without_arguments(capsys):
-
-    with pytest.raises(DocoptExit):
-        apg_main()
+    """Verify that apg called without args aserts DocOptExit."""
+    # with pytest.raises(DocoptExit):
+    #     apg_main()
+    apg_main()
+    captured = capsys.readouterr()
+    assert "Usage:" in captured.out
 
 
 def test_apg_main_with_args(
     phrase_path: Path,
     sound_path: Path,
     output_path: Path,
-    capsys,
 ):
+    """Verify calling main() outputs a file."""
     sys.argv = ["apg", str(phrase_path), str(sound_path)]
-
     apg_main()
 
     assert output_path.exists()
