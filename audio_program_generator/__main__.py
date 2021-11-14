019e9b990374ad446c3f6117726421092e5c1ecf
JnyJny <erik.oshaughnessy@gmail.com>
2021-11-14 10:42:08 -0600

    Created a new command-line interface based on typer
    Changes made to support new typer-based implementation:
    - added `typer` to installation requirements
    - added `ncli` to [tools.poetry.scripts] section in pyproject.toml
    - created audio_program_generator/__main__.py to house new CLI
    
    The new CLI uses [typer][1] to specify the arguments handled
    by the cli as well as docstrings which will appear when an
    error occurs while parsing the arguments supplied by the user
    as well as when the user asks for help with `--help`.
    
    The outline of a typer-based implementation is first create a
    `typer.Typer()` instance which will stand in for our `main()`
    function. Next, attach functions to the `Typer` instance using
    decorators (i.e. @cli.command), and the options to that function
    will specify it's command-line options.
    
    1: https://typer.tiangolo.com

---
+++ audio_program_generator/__main__.py
@@ -0,0 +1,129 @@
+""" command-line interface
+
+"""
+
+from pathlib import Path
+from enum import Enum
+
+import typer
+
+from .apg import AudioProgramGenerator
+
+cli = typer.Typer()
+
+
+class RegionalAccent(str, Enum):
+    AU: str = "AU"
+    CA: str = "CA"
+    IE: str = "IE"
+    IN: str = "IN"
+    UK: str = "UK"
+    US: str = "US"
+    ZA: str = "ZA"
+
+    @classmethod
+    def get_tld(cls, region: str) -> str:
+        return {
+            "au": "com.au",
+            "ca": "ca",
+            "ie": "ie",
+            "in": "co.in",
+            "uk": "co.uk",
+            "us": "com",
+            "za": "co.za",
+        }.get(region.lower())
+
+
+@cli.command()
+def generate_subcommand(
+    phrase_path: Path,
+    output_path: Path = typer.Option(
+        None,
+        "--output-path",
+        "-o",
+        show_default=True,
+        help="Path to store resulting MP3 audio file.",
+    ),
+    sound_path: Path = typer.Option(
+        None,
+        "--sound-path",
+        "-s",
+        show_default=True,
+        help="Path for an optional WAV to mix with the generated speech.",
+    ),
+    attenuation: int = typer.Option(
+        0,
+        "--attenuation",
+        "-a",
+        show_default=True,
+        help="Set background file attenuation in dB",
+    ),
+    slow: bool = typer.Option(
+        False,
+        "--slow",
+        is_flag=True,
+        show_default=True,
+        help="Generate speech at half-speed.",
+    ),
+    regional_accent: RegionalAccent = typer.Option(
+        RegionalAccent.US.value,
+        "--region",
+        "-r",
+        show_default=True,
+        help="Regional accent to apply to generated speech.",
+    ),
+    book_mode: bool = typer.Option(
+        False,
+        "--book-mode",
+        "-B",
+        is_flag=True,
+        show_default=True,
+        help="Operates on plain text without phrase pause formatting.",
+    ),
+    hide_progress_bar: bool = typer.Option(
+        False,
+        "--hide-progress-bar",
+        is_flag=True,
+        show_default=True,
+        help="Do not display progress bar during execution.",
+    ),
+) -> None:
+    """Generate audio program of spoken phrases, with optional background
+    sound file mixed in.
+
+    The user supplies a semicolon-separated text file with plain-text
+    phrases, each followed by an inter-phrase duration. Each line of
+    the file is comprised of:
+
+    - one phrase to be spoken
+    - a semicolon
+    - a silence duration (specified in seconds)
+
+    The script generates and saves a single MP3 file. If a path for
+    the output file is not given, the output file will have the same
+    base name as the input file with the suffix ".mp3" in the current
+    working directory.
+    """
+
+    if not output_path:
+        output_path = Path.cwd() / phrase_path.with_suffix(".mp3").name
+
+    try:
+        sound_file = sound_path.open("rb")
+    except AttributeError:
+        sound_file = None
+
+    with phrase_path.open("r") as phrase_file:
+        apGen = AudioProgramGenerator(
+            phrase_file,
+            sound_file,
+            slow=slow,
+            attenuation=attenuation,
+            tld=RegionalAccent.get_tld(regional_accent),
+            hide_progress_bar=hide_progress_bar,
+            book_mode=book_mode,
+        )
+        audio_data = apGen.invoke()
+
+    with output_path.open("wb") as output_file:
+        output_file.write(audio_data.read())
