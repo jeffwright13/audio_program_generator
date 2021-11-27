a50476088a59f00b563e362dd9b22f90f9dfc1d9
Jeff Wright <jeffrey.wright@wartsila.com>
2021-11-27 06:01:12 -0700

    Issue 47: replace tqdm with alive_progress.

--- audio_program_generator/apg.py
+++ audio_program_generator/apg.py
@@ -9,9 +9,9 @@ from io import StringIO, BytesIO
 from pathlib import Path
 from gtts import gTTS
 from pydub import AudioSegment
-from tqdm import tqdm
-
+from alive_progress import alive_bar, config_handler
 from audio_program_generator import cli
+
 
 def parse_textfile(phrase_file_contents: str = "") -> list:
     """
@@ -51,6 +51,14 @@ class AudioProgramGenerator:
         self.hide_progress_bar = kwargs.get("hide_progress_bar", False)
         self.book_mode = kwargs.get("book_mode", False)
 
+        config_handler.set_global(
+            bar=None,
+            stats=False,
+            monitor=False,
+            elapsed=False,
+            disable=self.hide_progress_bar,
+        )  # Set 'alive' progress bar global options
+
     def _gen_speech(self):
         """
         Generate a combined speech file, made up of gTTS-generated speech
@@ -60,45 +68,30 @@ class AudioProgramGenerator:
         combined = AudioSegment.empty()
 
         if self.book_mode:
-            for phrase in tqdm(
-                self.phrase_file.split(os.linesep), disable=self.hide_progress_bar
-            ):
-                # Skip blank phrases or gTTS will throw exception
-                if not phrase.strip():
-                    continue
-
-                # Write gTTS snippet to temp file for later access
-                tmpfile = BytesIO(None)
-                speech = gTTS(phrase, slow=self.slow, tld=self.tld)
-                speech.write_to_fp(tmpfile)
-                tmpfile.seek(0)
-
-                # Add the current speech snippet + corresponding silence
-                # to the combined file, building up for each new line.
-                snippet = AudioSegment.from_file(tmpfile, format="mp3")
-                combined += snippet
-                tmpfile.close()
+            phrases = self.phrase_file.split(os.linesep)
+            durations = [None for elem in range(len(phrases))]
+            items = list(zip(phrases, durations))
         else:
-            for phrase, duration in tqdm(
-                parse_textfile(self.phrase_file), disable=self.hide_progress_bar
-            ):
-                # Skip blank phrases or gTTS will throw exception
-                if not phrase.strip():
-                    continue
+            items = parse_textfile(self.phrase_file)
 
-                # Write gTTS snippet to temp file for later access
-                tmpfile = BytesIO(None)
-                speech = gTTS(phrase, slow=self.slow, tld=self.tld)
-                speech.write_to_fp(tmpfile)
-                tmpfile.seek(0)
+        for phr, dur in items:
+            # Skip blank phrases or gTTS will throw exception
+            if not phr.strip():
+                continue
 
-                # Add the current speech snippet + corresponding silence
-                # to the combined file, building up for each new line.
-                snippet = AudioSegment.from_file(tmpfile, format="mp3")
-                combined += snippet
-                silence = AudioSegment.silent(duration=1000 * int(duration))
-                combined += silence
-                tmpfile.close()
+            # Write gTTS snippet to temp file for later access
+            tmpfile = BytesIO(None)
+            speech = gTTS(phr, slow=self.slow, tld=self.tld)
+            speech.write_to_fp(tmpfile)
+            tmpfile.seek(0)
+
+            # Add the current speech snippet + corresponding silence
+            # to the combined file, building up for each new line.
+            snippet = AudioSegment.from_file(tmpfile, format="mp3")
+            combined += snippet
+            if dur:
+                combined += AudioSegment.silent(duration=1000 * int(dur))
+            tmpfile.close()
 
         self.speech_file = combined
 
@@ -129,13 +122,18 @@ class AudioProgramGenerator:
             (segment2_normalized - float(seg2_atten)).fade_in(fadein).fade_out(fadeout)
         )
 
-    def _validate_filename_extensions(self, phr_file: StringIO, snd_file: BytesIO) -> bool:
+    def _validate_filename_extensions(
+        self, phr_file: StringIO, snd_file: BytesIO
+    ) -> bool:
         """
         Verify attempted use of filenames
         """
 
         if snd_file:
-            return Path(snd_file.name).suffix == ".wav" and Path(phr_file.name).suffix == ".txt"
+            return (
+                Path(snd_file.name).suffix == ".wav"
+                and Path(phr_file.name).suffix == ".txt"
+            )
         return Path(phr_file.name).suffix == ".txt"
 
     def invoke(self) -> BytesIO:
@@ -143,17 +141,15 @@ class AudioProgramGenerator:
         Generate gTTS speech snippets for each phrase; optionally mix with
         background sound-file; then save resultant mp3.
         """
-
         assert self.filenames_valid
-        self._gen_speech()
-
-        if self.sound_file:
-            bkgnd = AudioSegment.from_file(self.sound_file, format="wav")
-            self.mix_file = self._mix(self.speech_file, bkgnd, self.attenuation)
-            self.mix_file.export(self.result, format="mp3")
-        else:
-            self.speech_file.export(self.result, format="mp3")
-
+        with alive_bar(0):
+            self._gen_speech()
+            if self.sound_file:
+                bkgnd = AudioSegment.from_file(self.sound_file, format="wav")
+                self.mix_file = self._mix(self.speech_file, bkgnd, self.attenuation)
+                self.mix_file.export(self.result, format="mp3")
+            else:
+                self.speech_file.export(self.result, format="mp3")
         return self.result
 
 
